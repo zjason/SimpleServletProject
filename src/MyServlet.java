4da872d694e227f71bf5b69df61e0397f61c8635
zjason <jason.zhao1992@gmail.com>
2017-05-05 09:38:23 +0800

    Add a simple servlet page

---
+++ src/MyServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by jason on 5/5/17.
+ */
+@WebServlet(name = "MyServlet")
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Hello from GET method");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h1>Simple servlet page</h1>");
+    }
+}
